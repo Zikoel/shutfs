35b06dcf48c065e657400007062293c9c1c469f2
Elia Mazzuoli <zikoel@gmail.com>
2020-04-21 07:46:50 +0200

    Removed unused type

--- server/src/SecondaryAdapter/LocalFileStorage/index.ts
+++ server/src/SecondaryAdapter/LocalFileStorage/index.ts
@@ -4,14 +4,6 @@ import { Fs } from 'Application/ports'
 
 const debug = D('secondaryAdapters:local-file-storage')
 
-export interface FileStorage {
-  createNewFile: (
-    fileName: string,
-    extension: string,
-    content: Buffer
-  ) => Promise<void>
-}
-
 export const createLocalFileStorage = (storagePath: string): Fs => {
   const files = fs.readdirSync(storagePath)
   debug(`There are ${files.length} on folder ${storagePath}`)
